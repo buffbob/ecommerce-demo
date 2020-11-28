e59de8354915044490d3a9e19d02c3222a2eb872
buffbob <lastgulch@gmail.com>
2020-11-28 09:09:33 -0800

    just setting up

---
+++ src/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div>
+        <div>
+            <div>
+                <h1>shit</h1>
+            </div>
+        </div>
+    </div>
+
+);
+
+
+export default HomePage;
+
