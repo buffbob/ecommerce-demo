ad756ff595544f2fe2f7927616f560805ff51f90
buffbob <lastgulch@gmail.com>
2020-11-28 10:54:25 -0800

    adding css files

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,16 +1,53 @@
 import React from 'react';
+import './homepage.styles.scss';
 
 const HomePage = () => (
-    <div>
-        <div>
-            <div>
-                <h1>shit</h1>
+    <div className='homepage'>
+        <h1 id="id1">Welcome</h1>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>HATS
+                        <span className='subtitle'>Shop</span>
+                    </div>
+                </div>
             </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>HATS
+                        <span className='subtitle'>Shop</span>
+                    </div>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>HATS
+                        <span className='subtitle'>Shop</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>COATS
+                        <span className='subtitle'>Shop</span>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className='menu-item'>
+                <div className='content'>
+                    <div className='title'>BOOTS
+                        <span className='subtitle'>Shop</span>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 
 );
-
 
 export default HomePage;
 
